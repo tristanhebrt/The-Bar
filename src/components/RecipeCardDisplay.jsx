2aa32af2ffca287405f862542f4dd6fe993d56e7
Tristan Hebert <91288854+tristanhebrt@users.noreply.github.com>
2025-03-15 06:16:23 -0400

    Bourbon Filter

--- src/components/RecipeCardDisplay.jsx
+++ src/components/RecipeCardDisplay.jsx
@@ -93,7 +93,7 @@ const RecipeCardDisplay = ({ mainTitle, recipes }) => {
             {/* Filter Dropdown */}
             {isFilterOpen && (
                 <FilterMenu>
-                    {["vodka", "gin", "rum", "whiskey", "tequila", "liqueur", "lime", "lemon", "simple", "agave", "demerara"].map(
+                    {["vodka", "gin", "rum", "whiskey", "bourbon", "tequila", "liqueur", "lime", "lemon", "simple", "agave", "demerara"].map(
                         (item, index) => (
                             <FilterOption key={index}>
                                 <label>
