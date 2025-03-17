7afcbc0a563d485342b28d3658ac31b3968799bc
Tristan Hebert <91288854+tristanhebrt@users.noreply.github.com>
2025-03-17 03:14:12 -0400

    Prep Checklist Update
    Added rags to the prep list

--- src/components/lists/prep/checklistData.js
+++ src/components/lists/prep/checklistData.js
@@ -50,7 +50,8 @@ export const PREP_STEPS = [
         title: "Top off Prebatch",
         content: {
         Prebatch: [
-            "Into the Fire", "Margarita Mix", "Boat Party", "Sunset Chaser", "Dreamer", "Secret Rose", "Bourbon Turnover"
+            "Into the Fire", "Margarita Mix", "Boat Party", "Sunset Chaser", "Dreamer", 
+            "Secret Rose", "Bourbon Turnover"
         ]
         }
     },
@@ -58,7 +59,8 @@ export const PREP_STEPS = [
         title: "Top off Syrups",
         content: {
         Syrups: [
-            "Lemon", "Lime", "Espresso", "Simple", "Agave", "Demerara", "Strawberry", "Thyme", "Crush", "Banana", "Spice", "Cinnamon"
+            "Lemon", "Lime", "Espresso", "Simple", "Agave", "Demerara", "Strawberry", 
+            "Thyme", "Crush", "Banana", "Spice", "Cinnamon"
         ]
         }
     },
@@ -66,7 +68,7 @@ export const PREP_STEPS = [
         title: "Final Checks",
         content: {
         items: [
-            "Skewers", "Paperclips", "Straws", "Napkins",
+            "Skewers", "Paperclips", "Straws", "Napkins", "Polishing rags", "Cleaning rags",
             "Candles", "Box for empties", "Ice", "Pebble ice", "King cubes"
         ]
         }
