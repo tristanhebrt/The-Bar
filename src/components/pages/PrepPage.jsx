1d6446c394231fd1c050a3fd9fa3c4c72e033c41
Tristan Hebert <91288854+tristanhebrt@users.noreply.github.com>
2025-03-16 23:26:40 -0400

    Coming Soon Prep

--- src/components/pages/PrepPage.jsx
+++ src/components/pages/PrepPage.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import styled from "styled-components";
 
 const PrepPage = () => {
-    return 
-    <>
-    
+    return<>
+        <ComingSoon>Coming Soon</ComingSoon>
     </>
 };
 
 export default PrepPage;
+
+const ComingSoon = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 3rem;
+
+    font-size: 3rem;
+    font-weight: 800;
+    color: var(--black);
+`
